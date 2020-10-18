4f71c3d21c4c6b7d6ec616239b7786dc05e6f6c4
Lcnvdl <programando.soft@gmail.com>
2020-10-18 17:53:05 -0300

    Initial commit

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+/** @note This file should contain all the library exports */
+/**
+ * @example
+ * export * from "./a";
+ * export * from "./b";
+ * export * from "./c";
+ * */
+//# sourceMappingURL=index.d.ts.map
